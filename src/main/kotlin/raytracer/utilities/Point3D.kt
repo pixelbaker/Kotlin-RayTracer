c520690d16fd8fc241e99b464996cbc3b57257d6
Pixelbaker <mail@pixelbaker.com>
2022-09-27 14:09:47 +0200

    Started adding Point3D

---
+++ src/main/kotlin/raytracer/utilities/Point3D.kt
@@ -0,0 +1,11 @@
+package raytracer.utilities
+
+class Point3D(
+    val x: Double = 0.0,
+    val y: Double = 0.0,
+    val z: Double = 0.0, ) {
+
+    constructor(a: Double): this(a, a, a)
+
+    constructor(p: Point3D): this(p.x, p.y, p.z)
+}
---
+++ src/test/kotlin/raytracer/utilities/Point3DTest.kt
@@ -0,0 +1,38 @@
+package raytracer.utilities
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class Point3DTest {
+    @Test
+    internal fun `default constructor`() {
+        val point = Point3D()
+        assertEquals(0.0, point.x)
+        assertEquals(0.0, point.y)
+        assertEquals(0.0, point.z)
+    }
+
+    @Test
+    internal fun `x, y and z is supplied`() {
+        val point = Point3D(x = 1.0, y = 2.0, z = 3.0)
+        assertEquals(1.0, point.x)
+        assertEquals(2.0, point.y)
+        assertEquals(3.0, point.z)
+    }
+
+    @Test
+    internal fun `one value to rule them all`() {
+        val point = Point3D(5.0)
+        assertEquals(5.0, point.x)
+        assertEquals(5.0, point.y)
+        assertEquals(5.0, point.z)
+    }
+
+    @Test
+    internal fun `copy constructor`() {
+        val point = Point3D(Point3D(1.0, 2.0, z = 3.0))
+        assertEquals(1.0, point.x)
+        assertEquals(2.0, point.y)
+        assertEquals(3.0, point.z)
+    }
+}
