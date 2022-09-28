47251d39bb238393532d788470f912136e29845f
Pixelbaker <mail@pixelbaker.com>
2022-09-28 20:12:58 +0200

    Point3D implemented

--- src/main/kotlin/raytracer/utilities/Point3D.kt
+++ src/main/kotlin/raytracer/utilities/Point3D.kt
@@ -1,5 +1,7 @@
 package raytracer.utilities
 
+import kotlin.math.sqrt
+
 class Point3D(
     val x: Double = 0.0,
     val y: Double = 0.0,
@@ -10,4 +12,22 @@ class Point3D(
     constructor(p: Point3D): this(p.x, p.y, p.z)
 
     operator fun unaryMinus() = Point3D(-x, -y, -z)
+
+    operator fun minus(p: Point3D) = Vector3D(x - p.x,y - p.y,z - p.z)
+
+    operator fun plus(v: Vector3D) = Point3D(x + v.x, y + v.y, z + v.z)
+
+    operator fun minus(p: Vector3D) = Point3D(x - p.x,y - p.y,z - p.z)
+
+    operator fun times(a: Double) = Point3D(x * a,y * a,z * a)
+
+    fun d_squared(p: Point3D) =
+        (x - p.x) * (x - p.x) +
+        (y - p.y) * (y - p.y) +
+        (z - p.z) * (z - p.z)
+
+    fun distance(p: Point3D) = sqrt((x - p.x) * (x - p.x) +
+        (y - p.y) * (y - p.y) +
+        (z - p.z) * (z - p.z))
+
 }
--- src/test/kotlin/raytracer/utilities/Point3DTest.kt
+++ src/test/kotlin/raytracer/utilities/Point3DTest.kt
@@ -43,4 +43,48 @@ internal class Point3DTest {
         assertEquals(-2.0, point.y)
         assertEquals(-3.0, point.z)
     }
+
+    @Test
+    internal fun `vector joining two points`() {
+        val vector = Point3D() - Point3D(1.0)
+        assertEquals(-1.0, vector.x)
+        assertEquals(-1.0, vector.y)
+        assertEquals(-1.0, vector.z)
+    }
+
+    @Test
+    internal fun `addition of a vector`() {
+        val point = Point3D() + Vector3D(1.0)
+        assertEquals(1.0, point.x)
+        assertEquals(1.0, point.y)
+        assertEquals(1.0, point.z)
+    }
+
+    @Test
+    internal fun `subtraction of a vector`() {
+        val point = Point3D() - Vector3D(1.0)
+        assertEquals(-1.0, point.x)
+        assertEquals(-1.0, point.y)
+        assertEquals(-1.0, point.z)
+    }
+
+    @Test
+    internal fun `multiplication by a double`() {
+        val point = Point3D(1.0) * 2.0
+        assertEquals(2.0, point.x)
+        assertEquals(2.0, point.y)
+        assertEquals(2.0, point.z)
+    }
+
+    @Test
+    internal fun `square of distance between two points`() {
+        val squareDistance = Point3D(2.0).d_squared(Point3D(1.0))
+        assertEquals(3.0, squareDistance)
+    }
+
+    @Test
+    internal fun `distance bewteen two points`() {
+        val squareDistance = Point3D(0.0).distance(Point3D(1.0))
+        assertEquals(1.7320, squareDistance, .01)
+    }
 }
