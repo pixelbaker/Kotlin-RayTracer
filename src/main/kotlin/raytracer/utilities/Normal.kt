f64f5cfae17518eb0d470078e9b90a5f9fd790a2
Pixelbaker <mail@pixelbaker.com>
2022-10-02 18:33:07 +0200

    Normal implemented

---
+++ src/main/kotlin/raytracer/utilities/Normal.kt
@@ -0,0 +1,42 @@
+package raytracer.utilities
+
+import kotlin.math.sqrt
+
+data class Normal(
+    var x: Double = 0.0,
+    var y: Double = 0.0,
+    var z: Double = 0.0,
+) {
+    constructor(a: Double) : this(a, a, a)
+
+    constructor(v: Vector3D) : this(v.x, v.y, v.z)
+
+    constructor(n: Normal) : this(n.x, n.y, n.z)
+
+    operator fun unaryMinus() = Normal(-x, -y, -z)
+
+    operator fun plus(n: Normal) = Normal(x + n.x, y + n.y, z + n.z)
+
+    operator fun plusAssign(n: Normal) {
+        x += n.x
+        y += n.y
+        z += n.z
+    }
+
+    /**
+     * Dot product with a vector on the right
+     */
+    operator fun times(v: Vector3D) = x * v.x + y * v.y + z * v.z
+    
+    operator fun times(a: Double) = Normal(x * a, y * a, z * a)
+
+    /**
+     * Convert normal to a unit normal
+     */
+    fun normalize() {
+        val length = sqrt(x * x + y * y + z * z)
+        x /= length
+        y /= length
+        z /= length
+    }
+}
---
+++ src/test/kotlin/raytracer/utilities/NormalTest.kt
@@ -0,0 +1,94 @@
+package raytracer.utilities
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+internal class NormalTest {
+    @Test
+    internal fun `default constructor`() {
+        val normal = Normal()
+        assertEquals(0.0, normal.x)
+        assertEquals(0.0, normal.y)
+        assertEquals(0.0, normal.z)
+    }
+
+    @Test
+    internal fun `x, y and z is supplied`() {
+        val normal = Normal(x = 1.0, y = 2.0, z = 3.0)
+        assertEquals(1.0, normal.x)
+        assertEquals(2.0, normal.y)
+        assertEquals(3.0, normal.z)
+    }
+
+    @Test
+    internal fun `one value to rule them all`() {
+        val normal = Normal(5.0)
+        assertEquals(5.0, normal.x)
+        assertEquals(5.0, normal.y)
+        assertEquals(5.0, normal.z)
+    }
+
+    @Test
+    internal fun `copy constructor`() {
+        val normal = Normal(Normal(1.0, 2.0, 3.0))
+        assertEquals(1.0, normal.x)
+        assertEquals(2.0, normal.y)
+        assertEquals(3.0, normal.z)
+    }
+
+    @Test
+    internal fun `construction by vector`() {
+        val normal = Normal(Vector3D(1.0, 2.0, z = 3.0))
+        assertEquals(1.0, normal.x)
+        assertEquals(2.0, normal.y)
+        assertEquals(3.0, normal.z)
+    }
+
+    @Test
+    internal fun `unary operator overloading`() {
+        val normal = -Normal(1.0, 2.0, z = 3.0)
+        assertEquals(-1.0, normal.x)
+        assertEquals(-2.0, normal.y)
+        assertEquals(-3.0, normal.z)
+    }
+
+    @Test
+    internal fun `multiplication by a double on the right`() {
+        val normal = Normal(1.0) * 3.0
+        assertEquals(3.0, normal.x)
+        assertEquals(3.0, normal.y)
+        assertEquals(3.0, normal.z)
+    }
+
+    @Test
+    internal fun `addition of two normals`() {
+        val normal = Normal(1.0) + Normal(1.0)
+        assertEquals(2.0, normal.x)
+        assertEquals(2.0, normal.y)
+        assertEquals(2.0, normal.z)
+    }
+
+    @Test
+    internal fun `compound addition`() {
+        val result = Normal(2.0)
+        result += Normal(1.0)
+        assertEquals(3.0, result.x)
+        assertEquals(3.0, result.y)
+        assertEquals(3.0, result.z)
+    }
+
+    @Test
+    internal fun `dot product with a vector on the right`() {
+        val dotProduct: Double = Normal(1.0) * Vector3D(1.0)
+        assertEquals(3.0, dotProduct)
+    }
+
+    @Test
+    internal fun `normalize normal`() {
+        val normal = Normal(1.0)
+        normal.normalize()
+        assertEquals(0.5773, normal.x, .0001)
+        assertEquals(0.5773, normal.y, .0001)
+        assertEquals(0.5773, normal.z, .0001)
+    }
+}
